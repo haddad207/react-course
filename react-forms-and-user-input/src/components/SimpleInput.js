a5770da21eb6891789c76bcfab52d7828be410e6
Fadi <haddad0207@gmail.com>
2021-05-30 15:39:55 +0300

    react router

---
+++ react-forms-and-user-input/src/components/SimpleInput.js
@@ -0,0 +1,89 @@
+import useInput from "../hooks/use-input";
+
+const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const mailFormat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.match(mailFormat) !== null);
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    if (!enteredEmailIsValid) {
+      return;
+    }
+
+    resetNameInput();
+    resetEmailInput();
+  };
+
+  const nameInputClasses = !nameInputHasError
+    ? "form-control"
+    : "form-control invalid";
+
+  const emailInputClasses = !emailInputHasError
+    ? "form-control"
+    : "form-control invalid";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameChangedHandler}
+          onBlur={nameBlurHandler}
+        />
+        {nameInputHasError && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Your Email</label>
+        <input
+          value={enteredEmail}
+          type="text"
+          id="name"
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailInputHasError && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
---
+++ react-router/src/components/MainHeader.js
@@ -0,0 +1,18 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ui>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+        </ui>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
